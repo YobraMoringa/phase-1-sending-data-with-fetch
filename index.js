b1610ddd01adc6377938838c61d3a161d500cf03
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-04-11 09:47:34 +0300

    Added a fetch method inside submitData function

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userNameInput, userEmailInput) {
+    const newUserObj = {
+        name: userNameInput,
+        email: userEmailInput
+    }
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newUserObj)
+    }
+    return fetch("http://localhost:3000/users", configurationObject).then(response => response.json()).then(newUserObj => {
+        const p = document.createElement("p");
+        p.append(newUserObj.id);
+        document.body.append(p);
+    }).catch(obj => {
+        const h1 = document.createElement("h1");
+        h1.append(obj.message);
+        document.body.append(h1);
+    })
+}
